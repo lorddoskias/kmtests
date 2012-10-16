5a88b0c8a8679a9baec2955d6eb42942210c7058
lorddoskias <lorddoskias@gmail.com>
2012-10-16 23:13:44 +0100

    WIP stuff for boundary cases

--- ntos_mm/ZwAllocateVirtualMemory.c
+++ ntos_mm/ZwAllocateVirtualMemory.c
@@ -12,6 +12,9 @@
 #define StartSeh()                  Status = STATUS_SUCCESS; _SEH2_TRY {
 #define EndSeh(ExpectedStatus)      } _SEH2_EXCEPT(EXCEPTION_EXECUTE_HANDLER) { Status = _SEH2_GetExceptionCode(); } _SEH2_END; ok_eq_hex(Status, ExpectedStatus)
 
+#define _2gb 0x80000000
+#define _1gb 0x40000000
+
 QUOTA_LIMITS limits;
 
 
@@ -31,7 +34,6 @@ static BOOLEAN CheckBuffer( PVOID Buffer, SIZE_T Size, UCHAR Value)
 
 		return TRUE;
 }
-
 
 static NTSTATUS CheckBufferReadWrite(PVOID Source, const PVOID Destination, SIZE_T Length) {
 	//do a little bit of writing/reading to memory
@@ -72,15 +74,14 @@ static NTSTATUS SimpleAllocation() {
 	PVOID base = NULL;
 	SIZE_T RegionSize = 200;
 
-	// allocate the memory
+	// commit
 	Status = ZwAllocateVirtualMemory(NtCurrentProcess(), &base, 0, &RegionSize, MEM_COMMIT, PAGE_READWRITE);
 	ok_eq_hex(Status, STATUS_SUCCESS);
 	ok_eq_size(RegionSize, 4096); //this should have resulted in a single-page allocation
 
 	//check for the zero-filled pages 
-	ok_bool_true(CheckBuffer(base, RegionSize, 0), "The buffer is not zero-filed");
+	ok_bool_true(CheckBuffer(base, RegionSize, 0), "The buffer is not zero-filled");
 	CheckBufferReadWrite(base, (PVOID)TestString, 200);
-
 
 	// try freeing
 	RegionSize = 0;
@@ -96,8 +97,21 @@ static NTSTATUS SimpleAllocation() {
 	CheckBufferReadWrite(base, (PVOID)TestString, 200);
 
 
+	//should be able to reserve less than 2 gigs
+	/*RegionSize = _1gb; //this should be a valid upper bound
+	base = (PVOID) NULL;
+	Status = ZwAllocateVirtualMemory(NtCurrentProcess(), &base, 0, &RegionSize, MEM_RESERVE, PAGE_READWRITE);
+	ok_eq_hex(Status, STATUS_SUCCESS);
+	
+	RegionSize = 0;
+	Status = ZwFreeVirtualMemory(NtCurrentProcess(), &base, &RegionSize, MEM_RELEASE);
+	ok_eq_hex(Status, STATUS_SUCCESS);
+	
+	*/
+
 	return Status;
 }
+
 
 static NTSTATUS CustomBaseAllocation() {
 
@@ -151,12 +165,21 @@ static NTSTATUS InvalidAllocations() {
 	Status = ZwAllocateVirtualMemory(NtCurrentProcess(), &base, 0, &RegionSize, (MEM_COMMIT | MEM_RESERVE), PAGE_READWRITE);
 	ok_eq_hex(Status, STATUS_INVALID_PARAMETER_2);
 
-	//allocate more than the architecturally allowed 2 gigabytes for a 32bit
-	RegionSize = limits.MaximumWorkingSetSize + 100;
+	//invalid reservation of 2 gigs
+/*
+	RegionSize = MM_HIGHEST_VAD_ADDRESS + 1; //this should be an invalid
 	base = (PVOID) NULL;
-	Status = ZwAllocateVirtualMemory(NtCurrentProcess(), &base, 0, &RegionSize, (MEM_COMMIT | MEM_RESERVE), PAGE_READWRITE);
-	ok_eq_hex(Status, STATUS_COMMITMENT_LIMIT);
+	Status = ZwAllocateVirtualMemory(NtCurrentProcess(), &base, 0, &RegionSize, MEM_RESERVE, PAGE_READWRITE);
+	ok_eq_hex(Status, STATUS_INVALID_PARAMETER_4);
+	//should be able to free a non-reserved memory
+	RegionSize = 0;
+	Status = ZwFreeVirtualMemory(NtCurrentProcess(), &base, &RegionSize, MEM_RELEASE);
+	ok_eq_hex(Status, STATUS_UNABLE_TO_FREE_VM);
+
+	*/
+
 	return Status;
+
 }
 
 
