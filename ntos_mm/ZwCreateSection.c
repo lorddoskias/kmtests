0f7e95a7f6f06220038ec7bc6e01f663453b946e
lorddoskias <lorddoskias@gmail.com>
2012-11-30 02:21:34 +0000

    Further implementation of tests

--- ntos_mm/ZwCreateSection.c
+++ ntos_mm/ZwCreateSection.c
@@ -8,6 +8,8 @@
 #include <kmt_test.h>
 
 #define IGNORE -999
+extern const char TestString[];
+extern const SIZE_T TestStringSize;
 
 #define CREATE_SECTION(Handle, DesiredAccess, Attributes, Size, SectionPageProtection, AllocationAttributes, FileHandle,  RetStatus, CloseRetStatus)  do    \
     {                                                                                                                                                       \
@@ -15,11 +17,48 @@
         ok_eq_hex(Status, RetStatus);                                                                                                                       \
         if(Handle != NULL)                                                                                                                                  \
         {                                                                                                                                                   \
-            Status = ZwClose(Handle);                                                                                                                       \
-            if (CloseRetStatus != IGNORE) ok_eq_hex(Status, CloseRetStatus);                                                                                \
+           Status = ZwClose(Handle);                                                                                                                        \
+           if (CloseRetStatus != IGNORE) ok_eq_hex(Status, CloseRetStatus);                                                                                 \
             Handle = NULL;                                                                                                                                  \
         }                                                                                                                                                   \
     } while (0)                                                                                                                                             \
+
+
+
+static 
+VOID
+KmtInitTestFiles(PHANDLE ReadOnlyFile, PHANDLE WriteOnlyFile) 
+{
+    NTSTATUS Status;
+    LARGE_INTEGER FileOffset;
+    UNICODE_STRING FileReadOnly = RTL_CONSTANT_STRING(L"\\SystemRoot\\system32\\ntdll.dll");
+    UNICODE_STRING FileWriteOnly = RTL_CONSTANT_STRING(L"\\SystemRoot\\kmtest-MmSection.txt");
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    IO_STATUS_BLOCK IoStatusBlock;
+    UCHAR FileData = 0;
+
+    //INIT THE READ-ONLY FILE
+    InitializeObjectAttributes(&ObjectAttributes, &FileReadOnly, OBJ_CASE_INSENSITIVE, NULL, NULL);
+    Status = ZwCreateFile(ReadOnlyFile, GENERIC_READ, &ObjectAttributes, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_OPEN, FILE_NON_DIRECTORY_FILE, NULL, 0);
+    ok_eq_hex(Status, STATUS_SUCCESS);
+    ok(*ReadOnlyFile != NULL, "Couldn't acquire READONLY handle\n");
+
+    //INIT THE WRITE-ONLY FILE
+    InitializeObjectAttributes(&ObjectAttributes, &FileWriteOnly, OBJ_CASE_INSENSITIVE, NULL, NULL);
+    Status = ZwCreateFile(WriteOnlyFile, GENERIC_WRITE | SYNCHRONIZE, &ObjectAttributes, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_SUPERSEDE, (FILE_NON_DIRECTORY_FILE | FILE_DELETE_ON_CLOSE), NULL, 0);
+    ok_eq_hex(Status, STATUS_SUCCESS);
+    ok_eq_ulongptr(IoStatusBlock.Information, FILE_CREATED);
+    ok(*WriteOnlyFile != NULL, "WriteOnlyFile is NULL\n");
+    if (WriteOnlyFile)
+    {
+        FileOffset.QuadPart = 0;
+        Status = ZwWriteFile(*WriteOnlyFile, NULL, NULL, NULL, &IoStatusBlock, (PVOID)TestString, TestStringSize, &FileOffset, NULL);
+        ok(Status == STATUS_SUCCESS || Status == STATUS_PENDING, "Status = 0x%08lx\n", Status);
+        Status = ZwWaitForSingleObject(*WriteOnlyFile, FALSE, NULL);
+        ok_eq_hex(Status, STATUS_SUCCESS);
+        ok_eq_ulongptr(IoStatusBlock.Information, TestStringSize);
+    }
+}
 
 static
 VOID 
@@ -27,11 +66,15 @@ SimpleErrorChecks(VOID)
 {
     NTSTATUS Status;
     HANDLE Section = NULL;
-    HANDLE FileHandle = NULL;
-    OBJECT_ATTRIBUTES ObjectAttributes;
+    HANDLE FileHandleReadOnly = NULL;
+    HANDLE FileHandleWriteOnly = NULL;
+    
+    OBJECT_ATTRIBUTES ObjectAttributesReadOnly;
+    OBJECT_ATTRIBUTES ObjectAttributesWriteOnly;
     LARGE_INTEGER MaximumSize;
-    UNICODE_STRING SectionName = RTL_CONSTANT_STRING(L"\\BaseNamedObjects\\KmtTestSection");
 
+    UNICODE_STRING SectReadOnly = RTL_CONSTANT_STRING(L"\\BaseNamedObjects\\KmtTestReadSect");
+    UNICODE_STRING SectWriteOnly = RTL_CONSTANT_STRING(L"\\BaseNamedObjects\\KmtTestWriteSect");
     MaximumSize.QuadPart = 200; 
 
     //PAGE FILE BACKED SECTION
@@ -71,6 +114,24 @@ SimpleErrorChecks(VOID)
     CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, MaximumSize, PAGE_READWRITE, (SEC_NOCACHE | SEC_COMMIT), NULL, STATUS_SUCCESS, STATUS_SUCCESS);
     CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, MaximumSize, PAGE_READWRITE, (SEC_NOCACHE | SEC_RESERVE), NULL, STATUS_SUCCESS, STATUS_SUCCESS);
 
+    //NORMAL FILE-BACKED SECTION
+    
+    //necessary init
+    KmtInitTestFiles(&FileHandleReadOnly, &FileHandleWriteOnly);
+
+    InitializeObjectAttributes(&ObjectAttributesReadOnly, &SectReadOnly, OBJ_CASE_INSENSITIVE, NULL, NULL);
+    InitializeObjectAttributes(&ObjectAttributesWriteOnly, &SectWriteOnly, OBJ_CASE_INSENSITIVE, NULL, NULL);
+
+    //DESIRED ACCESS TESTS 
+    CREATE_SECTION(Section, SECTION_ALL_ACCESS, &ObjectAttributesReadOnly, MaximumSize, PAGE_READONLY, SEC_COMMIT, FileHandleReadOnly, STATUS_SUCCESS, STATUS_SUCCESS);
+    CREATE_SECTION(Section, SECTION_ALL_ACCESS, &ObjectAttributesWriteOnly, MaximumSize, PAGE_WRITECOPY, SEC_COMMIT, FileHandleWriteOnly, STATUS_SUCCESS, STATUS_SUCCESS);
+
+
+    if(FileHandleReadOnly)
+        ZwClose(FileHandleReadOnly);
+
+    if(FileHandleWriteOnly)
+        ZwClose(FileHandleWriteOnly);
 }
 
 
