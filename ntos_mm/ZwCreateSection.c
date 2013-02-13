e7d595529d2dc19be75115937c1dc16a14b48f2b
lorddoskias <lorddoskias@gmail.com>
2013-02-13 20:36:12 +0000

    More section tests

--- ntos_mm/ZwCreateSection.c
+++ ntos_mm/ZwCreateSection.c
@@ -76,7 +76,7 @@ KmtInitTestFiles(PHANDLE ReadOnlyFile, PHANDLE WriteOnlyFile)
     ok(*ReadOnlyFile != NULL, "Couldn't acquire READONLY handle\n");
 
     //INIT THE WRITE-ONLY FILE
-    //TODO: Delete the file when the tests are all executed
+    //NB: this file is deleted at the end of basic behavior checks
     Status = ZwCreateFile(WriteOnlyFile, (GENERIC_WRITE | SYNCHRONIZE), &KmtestFileObject, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_WRITE, FILE_SUPERSEDE, FILE_NON_DIRECTORY_FILE, NULL, 0);
     ok_eq_hex(Status, STATUS_SUCCESS);
     ok_eq_ulongptr(IoStatusBlock.Information, FILE_CREATED);
@@ -132,7 +132,7 @@ SimpleErrorChecks(HANDLE FileHandleReadOnly, HANDLE FileHandleWriteOnly)
     CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, MaximumSize, PAGE_READWRITE, SEC_COMMIT, NULL, STATUS_INVALID_PARAMETER_4, IGNORE);
    
     //division by zero in ROS
-    if (SharedUserData->LargePageMinimum > 0 ) 
+    if (!skip((SharedUserData->LargePageMinimum > 0), "LargePageMinimum is 0")) 
     {
         MaximumSize.QuadPart = (_4mb / SharedUserData->LargePageMinimum) * SharedUserData->LargePageMinimum; //4mb 
         CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, MaximumSize, PAGE_READWRITE, (SEC_LARGE_PAGES | SEC_COMMIT), NULL, STATUS_SUCCESS, STATUS_SUCCESS);
@@ -188,7 +188,7 @@ SimpleErrorChecks(HANDLE FileHandleReadOnly, HANDLE FileHandleWriteOnly)
     
     MaximumSize.QuadPart = 0;
     CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, MaximumSize, PAGE_READONLY, SEC_COMMIT, FileHandleReadOnly, STATUS_SUCCESS, STATUS_SUCCESS);
-    
+     
     //allocation type
     CREATE_SECTION(Section, SECTION_MAP_READ, &ObjectAttributesWriteOnly, MaximumSize, PAGE_WRITECOPY, SEC_IMAGE, FileHandleWriteOnly, STATUS_INVALID_IMAGE_NOT_MZ, STATUS_SUCCESS);
 
@@ -213,41 +213,53 @@ BasicBehaviorChecks(VOID)
     HANDLE Section = NULL;
     HANDLE FileHandle;
     IO_STATUS_BLOCK IoStatusBlock;
-    PFILE_OBJECT File;
+    PFILE_OBJECT FileObject;
     LARGE_INTEGER Length;
     Length.QuadPart = TestStringSize;
 
 
-    //mimic lack of section support for a particular file.
-    Status = ZwCreateFile(&FileHandle, GENERIC_READ, &KmtestFileObject, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, FILE_NON_DIRECTORY_FILE, NULL, 0);
     
+    Status = ZwCreateFile(&FileHandle, GENERIC_READ, &KmtestFileObject, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, FILE_NON_DIRECTORY_FILE, NULL, 0);    
     if (NT_SUCCESS(Status))
     {
-        Status = ObReferenceObjectByHandle(FileHandle, STANDARD_RIGHTS_ALL, IoFileObjectType, KernelMode, &File, NULL);
+        
+        Status = ObReferenceObjectByHandle(FileHandle, STANDARD_RIGHTS_ALL, IoFileObjectType, KernelMode, &FileObject, NULL);
         if (NT_SUCCESS(Status))  
-        {
-            
-            PSECTION_OBJECT_POINTERS Pointers = File->SectionObjectPointer;
+        {    
+            //mimic lack of section support for a particular file as well.
+            PSECTION_OBJECT_POINTERS Pointers = FileObject->SectionObjectPointer;
 
-            File->SectionObjectPointer = NULL;
+            FileObject->SectionObjectPointer = NULL;
             CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, Length, PAGE_READONLY, SEC_COMMIT, FileHandle, STATUS_INVALID_FILE_FOR_SECTION, IGNORE);
-            File->SectionObjectPointer = Pointers;
-            ObDereferenceObject(File);
+            FileObject->SectionObjectPointer = Pointers;
+            ObDereferenceObject(FileObject);
         }
-
         ZwClose(FileHandle);
     }
 
-    //check normal section query 
+    //section creation with sizes different than those of the file
     Status = ZwCreateFile(&FileHandle, GENERIC_READ, &KmtestFileObject, &IoStatusBlock, NULL, FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, (FILE_NON_DIRECTORY_FILE | FILE_DELETE_ON_CLOSE), NULL, 0); 
     if(NT_SUCCESS(Status))
     {
+       Length.QuadPart = TestStringSize;
        CREATE_SECTION(Section, (SECTION_ALL_ACCESS), NULL, Length, PAGE_READONLY, SEC_COMMIT, FileHandle, STATUS_SUCCESS, NO_HANDLE_CLOSE);
        CheckObject(Section, 2, 1);
        CheckSection(Section, SEC_FILE, Length.QuadPart, STATUS_SUCCESS);
+       ZwClose(Section); //manually close it due to NO_HANDLE_CLOSE in CREATE_SECTION
+
+       //section length should be set to that of file
+       Length.QuadPart = 0;
+       CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, Length, PAGE_READONLY, SEC_COMMIT, FileHandle, STATUS_SUCCESS, NO_HANDLE_CLOSE);
+       CheckSection(Section, SEC_FILE, TestStringSize, STATUS_SUCCESS);
+       ZwClose(Section);
+
+       //create a smaller section than file
+       Length.QuadPart = TestStringSize - 100; 
+       CREATE_SECTION(Section, SECTION_ALL_ACCESS, NULL, Length, PAGE_READONLY, SEC_COMMIT, FileHandle, STATUS_SUCCESS, NO_HANDLE_CLOSE);
+       CheckSection(Section, SEC_FILE, TestStringSize - 100, STATUS_SUCCESS);
+       ZwClose(Section);
+
        ZwClose(FileHandle);
-       ZwClose(Section); //we have to manually close it due to NO_HANDLE_CLOSE in CREATE_SECTION
-       Section = NULL;
     }
 
     //check zero-based section
